47f9b13b46a898aaaf1a3080a4f3fe857c0806a5
Craig Fay <craigfay1@gmail.com>
2019-10-02 17:02:22 -0500

    WIP Allow file browsing

--- app/boundaries/serve-http.ts
+++ app/boundaries/serve-http.ts
@@ -4,6 +4,7 @@ import * as util from 'util';
 import * as UrlPattern from 'url-pattern';
 import * as formidable from 'formidable';
 const copy = util.promisify(fs.copyFile);
+const ls = util.promisify(fs.readdir);
 
 const { ASSETS_DIR, PORT } = process.env;
 
@@ -23,7 +24,10 @@ function matches(url:string, pattern:string): boolean {
 // Send a request to its corresponding handler function
 function requestRouter(req, res) {
   const { url, method } = req;
-  if (matches(url, '/api/upload') && method == 'POST') {
+  if (matches(url, '/api/uploads') && method == 'GET') {
+    return handleFileBrowse(req, res);
+  }
+  if (matches(url, '/api/uploads') && method == 'POST') {
     return handleFileUpload(req, res);
   }
   else {
@@ -39,6 +43,19 @@ async function handleFileUpload(req, res) {
     // Move the oploaded file from a temp location to a permanent location
     await copy(files.upload.path, `${ASSETS_DIR}/${field.title}`);
     res.writeHead(200);
-    res.end('ok');
+    res.end('OK');
   })
+}
+
+// Respond with a list of available files
+async function handleFileBrowse(req, res) {
+  try {
+    const files = await ls(ASSETS_DIR);
+    res.writeHead(200, { 'content-type':'application/json' });
+    res.end(JSON.stringify(files));
+  }
+  catch (e) {
+    res.writeHead(500);
+    res.end('Internal Server Error')
+  }
 }
